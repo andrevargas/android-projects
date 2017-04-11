41e8c7cb6a8d7cf598da4c08d51a092b25546051
André Vargas <vargas@geosapiens.com.br>
2017-04-11 17:36:58 -0300

    Removed duplicated code, renamed packages

--- Calculadora/app/src/androidTest/java/br/univali/sisnet/calculadora/ExampleInstrumentedTest.java
+++ Calculadora/app/src/androidTest/java/br/univali/sisnet/calculadora/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package sisnet.univali.br.calculadora;
+package br.univali.sisnet.calculadora;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("sisnet.univali.br.calculadora", appContext.getPackageName());
+        assertEquals("br.univali.sisnet.calculadora", appContext.getPackageName());
     }
 }
